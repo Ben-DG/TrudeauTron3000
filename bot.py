8001ccf66f584343c779557b2dd5aeb12fbcdedc
bendg <bendgrafstein@gmail.com>
2021-03-21 12:36:35 -0700

    Respond to all messages rather than commands

--- bot.py
+++ bot.py
@@ -4,32 +4,30 @@ import random
 import discord
 from dotenv import load_dotenv
 from os import listdir
-from discord.ext import commands
-
-image_path = "./trudeau_images/"
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+image_path = "./trudeau_images/"
+images_names = listdir(image_path)
 
 
-@bot.command(name='trudeau')
-async def post_pic(ctx):
-    trudeau_image_names = listdir(image_path)
-
-    image_name = random.choice(trudeau_image_names)
-    await ctx.send(file=discord.File(image_path + image_name))
-
-
-@bot.event
+@client.event
 async def on_ready():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
-        f'{bot.user} is connected to the following guild:\n'
+        f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
 
-bot.run(TOKEN)
+@client.event
+async def on_message(message):
+    if 'trudeau' in message.content.lower():
+        image_name = random.choice(images_names)
+        await message.channel.send(file=discord.File(image_path + image_name))
+
+
+client.run(TOKEN)
